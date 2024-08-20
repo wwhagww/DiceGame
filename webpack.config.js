a67d377a65aa9f755d2521b295cf08d314c837a6
wwhagww <167098228+wwhagww@users.noreply.github.com>
2024-08-20 14:58:07 +0000

    빌드 파일 삭제

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   module: {
